015b1846aeb0f5c495e2321dcfaca34179c59c74
Josh Pencheon <josh.pencheon@googlemail.com>
2021-03-26 10:38:00 +0000

    Update variables.tf

--- modules/pseudo-service/variables.tf
+++ modules/pseudo-service/variables.tf
@@ -1,11 +1,14 @@
 variable "release_tag" {
-  type    = string
+  type        = string
+  description = "The image tag to deploy." 
 }
 
 variable "label" {
-  type    = string
+  type        = string
+  description = "The identifier of the deployment. Used to name the Namespace, and the Ingress route"
 }
 
 variable "use_shared_db" {
-  type    = bool
+  type        = bool
+  description = "Whether to use a shared PG database, or deploy an ephemeral DB. Both will be migrated."
 }
